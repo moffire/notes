fded39694cffb1b92343c297d760f74fe8c06bb0
moffire <alexeydudarev.spb@gmail.com>
2020-08-17 20:24:43 +0300

    added absolute_url for *Post* objects

--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
         return super().get_queryset().filter(status='published')
+
 
 class Post(models.Model):
     STATUS_CHOICES = (
@@ -26,6 +28,9 @@ class Post(models.Model):
 
     class Meta:
         ordering = ('-publish',)
+
+    def get_absolute_url(self):
+        return reverse('post_detail', args=[self.publish.year, self.publish.month, self.publish.day, self.slug])
 
     def __str__(self):
         return self.title
