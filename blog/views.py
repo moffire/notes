d77f3e36bd06d7b25b02f7a2c862a0791697b2db
moffire <alexeydudarev.spb@gmail.com>
2020-08-18 20:51:37 +0300

    changed *Post* list display to CBV

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,13 @@
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.views.generic import ListView
 
-
-def post_list(request):
-    posts = Post.published.all()
-    return render(request, 'blog/post/list.html', context={'posts': posts})
+class PostLits(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 2
+    template_name = 'blog/post/list.html'
 
 
 def post_detail(request, year, month, day, post):
