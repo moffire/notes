1a5600bebbceeb0d9798272d5376d847a3d724ac
moffire <alexeydudarev.spb@gmail.com>
2020-08-19 20:13:33 +0300

    added ability to add a comments to the post

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from .models import Post, Comment
 from django.views.generic import ListView
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 from notes.settings import user_host
 
@@ -16,24 +16,35 @@ class PostList(ListView):
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post, status='published', publish__year=year,
                              publish__month=month, publish__day=day)
-    return render(request, 'blog/post/detail.html', context={'post': post})
+    comments = post.comments.filter(active=True)
+    new_comment = None
+    if request.method == 'POST':
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+    return render(request, 'blog/post/detail.html', context={'post': post,
+                                                             'comments': comments,
+                                                             'new_comment': new_comment,
+                                                             'comment_form': comment_form})
 
 
 def post_share(request, post_id):
     post = get_object_or_404(Post, id=post_id, status='published')
     sent = False
-    form = EmailPostForm()
     if request.method == 'POST':
         form = EmailPostForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
             post_url = request.build_absolute_uri(post.get_absolute_url())
             subject = '{0} ({1}) recommends you reading "{2}"'.format(cd['name'], cd['email'], post.title)
-            message = 'Read "{0}" at{1}\n\n{2}\'s comments: {1}'.format(post.title, post_url, cd['name'], cd['comments'])
+            message = 'Read "{0}" at{1}\n\n{2}\'s comments: {1}'.format(post.title, post_url, cd['name'],
+                                                                        cd['comments'])
             send_mail(subject, message, user_host, [cd['to']])
             sent = True
-            return render(request, 'blog/post/share.html', context={'post': post, 'form': form, 'sent': sent})
-        else:
-            return render(request, 'blog/post/share.html', context={'post': post, 'form': form, 'sent': sent})
     else:
-        return render(request, 'blog/post/share.html', context={'post': post, 'form': form, 'sent': sent})
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', context={'post': post, 'form': form, 'sent': sent})
